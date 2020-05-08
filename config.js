b52747e50f6efe371bc4dff04e9d80deaa91b6e6
Moataz Mohammady <moataz.mohammady@gmail.com>
2020-05-08 03:50:33 +0200

    enhance colors , typography

--- config.js
+++ config.js
@@ -1,325 +1,327 @@
 module.exports = {
-  siteTitle: 'Moataz Mohammady Resume', // <title>
-  manifestName: 'Resume',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: '', // This path is subpath of your hosting https://domain/portfolio
-  firstName: 'Moataz',
-  lastName: 'Mohammady',
-  description: `Full stack Web Developer with 4+ years of experience leveraging PHP & JavaScript to build responsive websites, and interactive features that drive business growth and improve UX.`,
-  education: [
-    {
-      place: "Cairo University",
-      url: "https://cu.edu.eg/Home",
-      title: "Bachelor's Degree in Commerce",
-      fromTO: '2008 - 2013',
-    }
-  ],
-  skills: [
-    'html5',
-    'css3-alt',
-    'js-square',
-    'react',
-    'node-js',
-    'sass',
-    'less',
-    'php',
-    'wordpress',
-    'gulp',
-    'grunt',
-    'npm',
-    'linux',
-    'git',
-  ],
-  workflow: [
-    'Mobile-First, Responsive Design',
-    'Cross Browser Testing & Debugging',
-    'Cross Functional Teams',
-    'Agile Development & Scrum',
-  ]
-  , interests: [
-    'Running',
-    'Reading',
-    'Cooking',
-  ],
-  // social
-  socialLinks: [
-    {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/MoatazAbdAlmageed',
-    },
-    {
-      icon: 'fa-linkedin-in',
-      name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/moatazabdelmageed',
-    },
-    {
-      icon: 'fa-behance',
-      name: 'Behance',
-      url: 'https://www.behance.net/moatazmohammady',
-    },
-    {
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/MoatazMohammady',
-    },
-    {
-      icon: 'fa-facebook-f',
-      name: 'Facebook',
-      url: 'https://www.facebook.com/moataz.mohammady.3',
-    },
-
-  ],
-  certifications: [
-    {
-      place: "Freecodecamp",
-      title: "Responsive Web Design",
-      description: "Developer Certification, representing approximately 300 hours of coursework",
-      url: "https://www.freecodecamp.org/certification/fcc319f679c/responsive-web-design",
-      issued: 'June 14, 2018'
-    },
-    {
-      place: "Udacity",
-      title: "Front End Development Track",
-      description: "One Million Arab Coders",
-      url: "https://s3-us-west-2.amazonaws.com/udacity-printer/production/certificates/8274026e-a9af-49ba-b82b-66b5824b65fe.pdf",
-      issued: 'May 2019'
-    }
-    ,
-    {
-      place: "Sololearn",
-      title: "HTML Fundamentals course",
-      url: "https://www.sololearn.com/Certificate/1014-1342830/pdf/",
-      issued: '06 January, 2017'
-    }, ,
-    {
-      place: "Sololearn",
-      title: "PHP Fundamentals course",
-      url: "https://www.sololearn.com/Certificate/1059-1342830/pdf/",
-      issued: '17 July, 2016'
-    }, ,
-    {
-      place: "Sololearn",
-      title: "JavaScript Fundamentals course",
-      url: "https://www.sololearn.com/Certificate/1024-1342830/pdf/",
-      issued: '15 September, 2016'
-    }, {
-      place: "futurelearn.com",
-      title: "BASIC ENGLISH 1: ELEMENTARY KING'S COLLEGE LONDON",
-      description: "This course helped you develop your use of English in a range of everyday situations and built your confidence by improving your listening skills. You’ve heard people talking in a number of different situations and have had an opportunity to review the language through a series of quizzes.",
-      url: "https://www.futurelearn.com/certificates/tl15gwi",
-      issued: '2019'
-    },
-    {
-      place: "futurelearn.com",
-      title: "BASIC ENGLISH 2: PRE-INTERMEDIATE KING'S COLLEGE LONDON",
-      description: "This online course helped you to demonstrate understanding of language that can help you progress along the path to studying at a university.",
-      url: "https://www.futurelearn.com/certificates/d829clk",
-      issued: '2019'
-    },
-  ],
-  email: 'moataz.mohammady@gmail.com',
-  phone: '+201150064746',
-  address: 'Mansouria,Giza,Egypt',
-  location: 'https://goo.gl/maps/QHh4H4eYJMByuneC8',
-  jobs: [
-    {
-      company: "Wuilt",
-      url: "http://wuilt.com/",
-      fromTO: 'Jan 2020 - Present',
-      occupation: "Full-Stack Web Developer",
-      responsibilities: [
-        "Develop new user-facing features",
-        "Build reusable code and libraries for future use",
-        "Collaborate with other team members and stakeholders",
-      ],
-      description: "",
-      projects: [
-      ]
-
-    },
-    {
-      company: "camelCase",
-      responsibilities: [
-        "Develop new user-facing features",
-        "Build reusable code and libraries for future use",
-        "Designing and managing the website back-end including database and server integration.",
-      ],
-      url: "https://camelcasetech.com/",
-      fromTO: 'May 2018 - Dec 2019',
-      occupation: "Full-Stack PHP Developer",
-      description: "",
-      projects: [
+    siteTitle: 'Moataz Mohammady Resume', // <title>
+    manifestName: 'Resume',
+    manifestShortName: 'Landing', // max 12 characters
+    manifestStartUrl: '/',
+    manifestBackgroundColor: '#663399',
+    manifestThemeColor: '#663399',
+    manifestDisplay: 'standalone',
+    manifestIcon: 'src/assets/img/website-icon.png',
+    pathPrefix: '', // This path is subpath of your hosting https://domain/portfolio
+    firstName: 'Moataz',
+    lastName: 'Mohammady',
+    description: `Full stack Web Developer with 4+ years of experience leveraging PHP & JavaScript to build responsive websites, and interactive features that drive business growth and improve UX.`,
+    education: [
         {
-          name: 'skilledmatrix.com',
-          url: "http://skilledmatrix.com/",
-          technologies: [
-            'HTML',
-            'CSS',
-            'JS',
-            'PHP',
-            'Laravel',
-          ],
+            place: "Cairo University",
+            url: "https://cu.edu.eg/Home",
+            title: "Bachelor's Degree in Commerce",
+            fromTO: '2008 - 2013',
+        }
+    ],
+    skills: [
+        'html5',
+        'css3-alt',
+        'js-square',
+        'react',
+        'node-js',
+        'sass',
+        'less',
+        'php',
+        'wordpress',
+        'gulp',
+        'grunt',
+        'npm',
+        'linux',
+        'git',
+    ],
+    workflow: [
+        'Mobile-First, Responsive Design',
+        'Cross Browser Testing & Debugging',
+        'Cross Functional Teams',
+        'Agile Development & Scrum',
+    ]
+    , interests: [
+        'Running',
+        'Reading',
+        'Cooking',
+    ],
+    // social
+    socialLinks: [
+        {
+            icon: 'fa-github',
+            name: 'Github',
+            url: 'https://github.com/MoatazAbdAlmageed',
+        },
+        {
+            icon: 'fa-linkedin-in',
+            name: 'Linkedin',
+            url: 'https://www.linkedin.com/in/moatazabdelmageed',
+        },
+        {
+            icon: 'fa-behance',
+            name: 'Behance',
+            url: 'https://www.behance.net/moatazmohammady',
+        },
+        {
+            icon: 'fa-twitter',
+            name: 'Twitter',
+            url: 'https://twitter.com/MoatazMohammady',
+        },
+        {
+            icon: 'fa-facebook-f',
+            name: 'Facebook',
+            url: 'https://www.facebook.com/moataz.mohammady.3',
+        },
+
+    ],
+    certifications: [
+        {
+            place: "Udacity",
+            title: "Front End Development Track",
+            description: "One Million Arab Coders",
+            url: "https://s3-us-west-2.amazonaws.com/udacity-printer/production/certificates/8274026e-a9af-49ba-b82b-66b5824b65fe.pdf",
+            issued: 'May 2019'
+        },
+        {
+            place: "futurelearn.com",
+            title: "BASIC ENGLISH 1: ELEMENTARY KING'S COLLEGE LONDON",
+            description: "This course helped you develop your use of English in a range of everyday situations and built your confidence by improving your listening skills. You’ve heard people talking in a number of different situations and have had an opportunity to review the language through a series of quizzes.",
+            url: "https://www.futurelearn.com/certificates/tl15gwi",
+            issued: '2019'
+        },
+        {
+            place: "futurelearn.com",
+            title: "BASIC ENGLISH 2: PRE-INTERMEDIATE KING'S COLLEGE LONDON",
+            description: "This online course helped you to demonstrate understanding of language that can help you progress along the path to studying at a university.",
+            url: "https://www.futurelearn.com/certificates/d829clk",
+            issued: '2019'
+        },
+        {
+            place: "Freecodecamp",
+            title: "Responsive Web Design",
+            description: "Developer Certification, representing approximately 300 hours of coursework",
+            url: "https://www.freecodecamp.org/certification/fcc319f679c/responsive-web-design",
+            issued: 'June 14, 2018'
+        },
+
+        ,
+        {
+            place: "Sololearn",
+            title: "HTML Fundamentals course",
+            url: "https://www.sololearn.com/Certificate/1014-1342830/pdf/",
+            issued: '06 January, 2017'
+        }, ,
+        {
+            place: "Sololearn",
+            title: "PHP Fundamentals course",
+            url: "https://www.sololearn.com/Certificate/1059-1342830/pdf/",
+            issued: '17 July, 2016'
+        }, ,
+        {
+            place: "Sololearn",
+            title: "JavaScript Fundamentals course",
+            url: "https://www.sololearn.com/Certificate/1024-1342830/pdf/",
+            issued: '15 September, 2016'
+        },
+    ],
+    email: 'moataz.mohammady@gmail.com',
+    phone: '+201150064746',
+    address: 'Mansouria,Giza,Egypt',
+    location: 'https://goo.gl/maps/QHh4H4eYJMByuneC8',
+    jobs: [
+        {
+            company: "Wuilt",
+            url: "http://wuilt.com/",
+            fromTO: 'Jan 2020 - Present',
+            occupation: "Full-Stack Web Developer",
+            responsibilities: [
+                "Develop new user-facing features",
+                "Build reusable code and libraries for future use",
+                "Collaborate with other team members and stakeholders",
+            ],
+            description: "",
+            projects: [
+            ]
+
+        },
+        {
+            company: "camelCase",
+            responsibilities: [
+                "Develop new user-facing features",
+                "Build reusable code and libraries for future use",
+                "Designing and managing the website back-end including database and server integration.",
+            ],
+            url: "https://camelcasetech.com/",
+            fromTO: 'May 2018 - Dec 2019',
+            occupation: "Full-Stack PHP Developer",
+            description: "",
+            projects: [
+                {
+                    name: 'skilledmatrix.com',
+                    url: "http://skilledmatrix.com/",
+                    technologies: [
+                        'HTML',
+                        'CSS',
+                        'JS',
+                        'PHP',
+                        'Laravel',
+                    ],
+                },
+
+                {
+                    name: 'skillsbuilderpro.com',
+                    url: "http://skillsbuilderpro.com/",
+
+                    technologies: [
+                        'HTML',
+                        'CSS',
+                        'JS',
+                        'PHP',
+                        'Zend',],
+                }, {
+                    name: 'knowledgeworkerspro.com',
+                    url: "http://knowledgeworkerspro.com/",
+
+
+                    technologies: [
+                        'HTML',
+                        'CSS',
+                        'JS',
+                        'PHP',
+                        'Zend',
+                    ],
+                },
+            ],
+
         },
 
         {
-          name: 'skillsbuilderpro.com',
-          url: "http://skillsbuilderpro.com/",
+            company: "Nile Bits",
+            url: "https://www.nilebits.com/",
+            fromTO: 'Jan 2017 - Apr 2018',
+            occupation: "Front-End developer",
+            responsibilities: [
+                "Created custom AngularJS components for internal framework",
+                "Build reusable code and libraries for future use",
+                "Collaborate with other team members and stakeholders",
+            ],
+            description: "",
+            projects: [
+                {
+                    name: 'netscribe',
+                    url: "https://viqsolutions.com/solutions/netscribe/",
 
-          technologies: [
-            'HTML',
-            'CSS',
-            'JS',
-            'PHP',
-            'Zend',],
+
+                    technologies: [
+                        'HTML',
+                        'CSS',
+                        'JS',
+                        'angularjs'
+                    ],
+                },
+            ],
+
+
+        },
+
+        {
+            company: "TIE",
+            url: "http://tie.com.eg/",
+            fromTO: 'Sep 2016 - Jan 2017',
+            occupation: "WordPress developer",
+            responsibilities: [
+                "Generating WordPress themes and plugins.",
+                "Troubleshooting content issues.",
+                "Conducting website performance tests.",
+            ],
+            description: "",
+            projects: [],
+
+
+        },
+
+        {
+            company: "EgyCoders",
+            url: "http://www.egycoders.com/",
+            fromTO: 'Mar 2016 - Jun 2016',
+            occupation: "Front-End developer",
+            responsibilities: [
+                "Develop new user-facing features",
+                "Assure that all user input is validated before submitting to back-end"
+            ],
+            description: "",
+            projects: [],
+
+
         }, {
-          name: 'knowledgeworkerspro.com',
-          url: "http://knowledgeworkerspro.com/",
+            company: "Orchtech",
+            url: "https://www.orchtech.com/",
+            fromTO: 'Dec 2015 - May 2016',
+            occupation: "WordPress Developer",
+            responsibilities: [
+                "Generating WordPress themes and plugins.",
+                "Troubleshooting content issues.",
+                "Conducting website performance tests.",
+            ],
+            description: "",
+            projects: [
+                {
+                    name: 'sevensprings.ch',
+                    url: "https://sevensprings.ch/",
 
 
-          technologies: [
-            'HTML',
-            'CSS',
-            'JS',
-            'PHP',
-            'Zend',
-          ],
+                    technologies: [
+                        'HTML',
+                        'CSS',
+                        'JS',
+                        'PHP',
+                        'Wordpress'
+                    ],
+                },
+                {
+                    name: 'shapwasco.com',
+                    url: "http://shapwasco.com.eg",
+
+                    technologies: [
+                        'HTML',
+                        'CSS',
+                        'JS',
+                        'PHP',
+                        'Wordpress'
+                    ],
+                },
+                {
+                    name: 'uacciap.org',
+                    url: "http://uacciap.org",
+
+
+                    technologies: [
+                        'HTML',
+                        'CSS',
+                        'JS',
+                        'PHP',
+                        'Wordpress'
+                    ],
+                },
+            ],
+
+        }, {
+            company: "Leogami",
+            url: "http://leogami.com/",
+            fromTO: 'Oct 2015 - Nov 2015',
+            occupation: "WordPress Developer",
+            responsibilities: [
+                "Designing and building the website front-end.",
+                "Creating the website architecture.",
+                "Generating WordPress themes and plugins.",
+            ],
+            description: "",
+            projects: [],
+
+
         },
-      ],
-
-    },
-
-    {
-      company: "Nile Bits",
-      url: "https://www.nilebits.com/",
-      fromTO: 'Jan 2017 - Apr 2018',
-      occupation: "Front-End developer",
-      responsibilities: [
-        "Created custom AngularJS components for internal framework",
-        "Build reusable code and libraries for future use",
-        "Collaborate with other team members and stakeholders",
-      ],
-      description: "",
-      projects: [
-        {
-          name: 'netscribe',
-          url: "https://viqsolutions.com/solutions/netscribe/",
-
-
-          technologies: [
-            'HTML',
-            'CSS',
-            'JS',
-            'angularjs'
-          ],
-        },
-      ],
-
-
-    },
-
-    {
-      company: "TIE",
-      url: "http://tie.com.eg/",
-      fromTO: 'Sep 2016 - Jan 2017',
-      occupation: "WordPress developer",
-      responsibilities: [
-        "Generating WordPress themes and plugins.",
-        "Troubleshooting content issues.",
-        "Conducting website performance tests.",
-      ],
-      description: "",
-      projects: [],
-
-
-    },
-
-    {
-      company: "EgyCoders",
-      url: "http://www.egycoders.com/",
-      fromTO: 'Mar 2016 - Jun 2016',
-      occupation: "Front-End developer",
-      responsibilities: [
-        "Develop new user-facing features",
-        "Assure that all user input is validated before submitting to back-end"
-      ],
-      description: "",
-      projects: [],
-
-
-    }, {
-      company: "Orchtech",
-      url: "https://www.orchtech.com/",
-      fromTO: 'Dec 2015 - May 2016',
-      occupation: "WordPress Developer",
-      responsibilities: [
-        "Generating WordPress themes and plugins.",
-        "Troubleshooting content issues.",
-        "Conducting website performance tests.",
-      ],
-      description: "",
-      projects: [
-        {
-          name: 'sevensprings.ch',
-          url: "https://sevensprings.ch/",
-
-
-          technologies: [
-            'HTML',
-            'CSS',
-            'JS',
-            'PHP',
-            'Wordpress'
-          ],
-        },
-        {
-          name: 'shapwasco.com',
-          url: "http://shapwasco.com.eg",
-
-          technologies: [
-            'HTML',
-            'CSS',
-            'JS',
-            'PHP',
-            'Wordpress'
-          ],
-        },
-        {
-          name: 'uacciap.org',
-          url: "http://uacciap.org",
-
-
-          technologies: [
-            'HTML',
-            'CSS',
-            'JS',
-            'PHP',
-            'Wordpress'
-          ],
-        },
-      ],
-
-    }, {
-      company: "Leogami",
-      url: "http://leogami.com/",
-      fromTO: 'Oct 2015 - Nov 2015',
-      occupation: "WordPress Developer",
-      responsibilities: [
-        "Designing and building the website front-end.",
-        "Creating the website architecture.",
-        "Generating WordPress themes and plugins.",
-      ],
-      description: "",
-      projects: [],
-
-
-    },
-    /* ... */
-  ],
+        /* ... */
+    ],
 
 };
