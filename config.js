d5b61c9e30e40e351b3c78151ff14497abd749ba
Moataz Mohammady <moataz.mohammady@gmail.com>
2020-05-08 04:18:09 +0200

    update manifestBackgroundColor

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     siteTitle: 'Moataz Mohammady Resume', // <title>
     manifestName: 'Resume',
-    manifestShortName: 'Landing', // max 12 characters
+    manifestShortName: 'Moataz Mohammady', // max 12 characters
     manifestStartUrl: '/',
-    manifestBackgroundColor: '#663399',
-    manifestThemeColor: '#663399',
+    manifestBackgroundColor: '#28a745',
+    manifestThemeColor: '#28a745',
     manifestDisplay: 'standalone',
     manifestIcon: 'src/assets/img/website-icon.png',
     pathPrefix: '', // This path is subpath of your hosting https://domain/portfolio
