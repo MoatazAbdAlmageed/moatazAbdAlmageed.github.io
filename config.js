b9bf43c36887b193c8138e57c743054621acace1
Moataz Mohammady <moataz@wuilt.com>
2020-05-14 13:00:17 +0200

    change colors , fix nav collapse

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
 	manifestName: 'Resume',
 	manifestShortName: 'Moataz Mohammady', // max 12 characters
 	manifestStartUrl: 'https://moatazabdalmageed.github.io/',
-	manifestBackgroundColor: '#0d0d0d',
-	manifestThemeColor: '#0d0d0d',
+	manifestBackgroundColor: '#f20505',
+	manifestThemeColor: '#f20505',
 	manifestDisplay: 'standalone',
 	manifestIcon: 'src/assets/img/website-icon.png',
 	pathPrefix: '', // This path is subpath of your hosting https://domain/portfolio
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,22 @@
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
+const $ = require('jquery');
+
+export const onInitialClientRender = () => {
+  $(document).ready(function() {
+    $('.navbar-collapse a').click(function(e) {
+      $('.navbar-collapse').collapse('toggle');
+    });
+  });
+};
 
 export const onServiceWorkerUpdateReady = () => {
-	const answer = window.confirm(`This application has been updated. ` + `Reload to display the latest version?`);
-	if (answer === true) {
-		window.location.reload();
-	}
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
 };
