9b62f0464bff66c983d1023ea5c4de0bc31788c3
Moataz Mohammady <moataz@wuilt.com>
2020-05-14 11:22:21 +0200

    fix nav

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 export const onServiceWorkerUpdateReady = () => {
 	const answer = window.confirm(`This application has been updated. ` + `Reload to display the latest version?`);
 	if (answer === true) {
