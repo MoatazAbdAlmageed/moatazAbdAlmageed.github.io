d558ba70a346f7682cbe1ec7f8ea70dca9a9dd25
Moataz Mohammady <moataz@wuilt.com>
2020-05-21 14:52:09 +0200

    merge `INTERESTS & VOLUNTEERING`

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -3,32 +3,59 @@ import React, { Component } from 'react';
 import config from '../../config';
 
 class Interests extends Component {
-    render() {
-        const { interests } = config;
+  render() {
+    const { interests, volunteerActivities } = config;
 
-        return (
-            <section
-                className="resume-section p-3  d-flex align-items-center"
-                id="interests"
-            >
-                <div className="w-100">
-                    <h2 className="mb-5">
-                        <i className="fa fa-heart" /> Interests
+    return (
+      <section
+        className="resume-section p-3  d-flex align-items-center"
+        id="interests"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">
+            <i className="fa fa-heart" /> Interests
           </h2>
-                    <p>Apart from being a web developer, I enjoy</p>
-                    <ul>
-                        {interests.map(interest => {
-                            return <li key={Math.random()}>
-                                <a href={interest.url} target="_blank" rel="noopener noreferrer">
-                                    {interest.title}
-                                </a>
-                            </li>;
-                        })}
-                    </ul>
-                </div>
-            </section>
-        );
-    }
+          <p>Apart from being a web developer, I enjoy</p>
+          <ul>
+            {interests.map(interest => {
+              return (
+                <li key={Math.random()}>
+                  <a
+                    href={interest.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {interest.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="w-100">
+          <h2 className="mb-5">
+            <i className="fa fa-smile" /> Volunteering
+          </h2>
+          <ul>
+            {volunteerActivities.map(activity => {
+              return (
+                <li key={Math.random()}>
+                  <a
+                    href={activity.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {activity.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Interests;
