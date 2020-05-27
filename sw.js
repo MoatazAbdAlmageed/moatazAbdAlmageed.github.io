b2f90eeeb94a86f53279f3c25c98b4f6026e0d20
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-27 21:24:58 +0000

    Deploy MoatazAbdAlmageed/moatazAbdAlmageed.github.io to github.com/MoatazAbdAlmageed/moatazAbdAlmageed.github.io.git:master

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9ef4c16a579f93420ac8.js"
+    "url": "webpack-runtime-39147b8f985800ffc0c6.js"
   },
   {
     "url": "framework-5dd3c501fd049989ea3a.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7f6bdce513d8e702cad6e7610bc1c239"
+    "revision": "af942bb85d943135569ae8232fb3a824"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-379649f957bef5221456.js"
