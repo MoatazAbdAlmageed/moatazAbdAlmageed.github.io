f49cfc878b197b07207c2d61242c730bd9e83fb0
MoatazAbdAlmageed <moataz@wuilt.com>
2020-09-13 11:04:39 +0200

    add `edabit`

--- config.js
+++ config.js
@@ -12,19 +12,32 @@ module.exports = {
   lastName: 'Mohammady',
   githubUsername: 'MoatazAbdAlmageed',
   position: 'Full stack Web Developer',
-  site_img:
-    'https://avatars2.githubusercontent.com/u/17373050?s=460&u=b26d5f5d825283b7b067e10a46add610b512cb26&v=4',
+  site_img: 'https://avatars2.githubusercontent.com/u/17373050?s=460&u=b26d5f5d825283b7b067e10a46add610b512cb26&v=4',
   description: `With 4+ years of experience leveraging PHP & JavaScript to build responsive websites.`,
   keywords: 'developer,web,code,programming,',
   TRACKING_ID: process.env.TRACKING_ID,
   GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   FORMSPREE: 'xgenoded', // create your form from => https://formspree.io
-  colors: [
-    { primary: '#252526', secondary: '#007acc' }, //vscode
-    { primary: '#4b4b4b', secondary: '#f09733' }, //sublime
-    { primary: '#4d4d4d', secondary: '#22a3dc' }, //brackets
-    { primary: '#24292e', secondary: '#2ea44f' }, //github
-    { primary: '#000000', secondary: '#e16259' }, //notion
+  colors: [{
+      primary: '#252526',
+      secondary: '#007acc'
+    }, //vscode
+    {
+      primary: '#4b4b4b',
+      secondary: '#f09733'
+    }, //sublime
+    {
+      primary: '#4d4d4d',
+      secondary: '#22a3dc'
+    }, //brackets
+    {
+      primary: '#24292e',
+      secondary: '#2ea44f'
+    }, //github
+    {
+      primary: '#000000',
+      secondary: '#e16259'
+    }, //notion
     // { primary: '#DC2167', secondary: '#0F1237' },
     // { primary: '#CD2731', secondary: '#96CBBB' },
     // { primary: '#E9C121', secondary: '#010101' },
@@ -44,19 +57,15 @@ module.exports = {
     // { primary: '#54BABF', secondary: '#ADDBD9' },
     // { primary: '#99BC44', secondary: '#DB2977' },
   ],
-  education: [
-    {
-      place: 'Cairo University',
-      url: 'https://cu.edu.eg/Home',
-      title: "Bachelor's Degree in Commerce",
-      period: '2008 - 2013',
-    },
-  ],
-  skills: [
-    {
+  education: [{
+    place: 'Cairo University',
+    url: 'https://cu.edu.eg/Home',
+    title: "Bachelor's Degree in Commerce",
+    period: '2008 - 2013',
+  }, ],
+  skills: [{
       title: 'Front-end',
-      skills: [
-        {
+      skills: [{
           title: 'HTML',
           icon: 'html5',
           url: 'https://en.wikipedia.org/wiki/HTML',
@@ -90,8 +99,7 @@ module.exports = {
     },
     {
       title: 'Back-end',
-      skills: [
-        {
+      skills: [{
           title: 'NodeJS',
           icon: 'node-js',
           url: 'https://nodejs.org/en/',
@@ -110,8 +118,7 @@ module.exports = {
     },
     {
       title: 'Database',
-      skills: [
-        {
+      skills: [{
           title: 'MySQL',
           icon: 'database fa',
           url: 'https://www.mysql.com/',
@@ -135,8 +142,7 @@ module.exports = {
     // },
     {
       title: 'Other',
-      skills: [
-        {
+      skills: [{
           title: 'Wordpress',
           icon: 'wordpress',
           url: 'https://wordpress.org/',
@@ -175,19 +181,15 @@ module.exports = {
     },
   ],
 
-  volunteerActivities: [
-    {
-      title: 'Resala Charity Organization | جمعية رسالة للأعمال الخيرية',
-      url: 'https://resala.org/',
-    },
-  ],
-  interests: [
-    {
+  volunteerActivities: [{
+    title: 'Resala Charity Organization | جمعية رسالة للأعمال الخيرية',
+    url: 'https://resala.org/',
+  }, ],
+  interests: [{
       url: '#',
       title: 'Blogging',
 
-      sites: [
-        {
+      sites: [{
           url: 'https://moatazworld.wordpress.com/',
           title: 'moatazworld',
         },
@@ -211,8 +213,7 @@ module.exports = {
       title: 'Running && Walking in the countryside with my family or friends.',
     },
   ],
-  socialLinks: [
-    {
+  socialLinks: [{
       icon: 'fa-github',
       name: 'Github',
       url: 'https://github.com/MoatazAbdAlmageed',
@@ -231,6 +232,11 @@ module.exports = {
       icon: 'fa-codepen',
       name: 'codepen',
       url: 'https://codepen.io/Moataz-Mohammady/pens/public',
+    },
+    {
+      icon: 'fa-trophy',
+      name: 'edabit',
+      url: 'https://edabit.com/user/NcL6egjCcAi5zR9k2',
     },
     {
       icon: 'fa-behance',
@@ -263,46 +269,39 @@ module.exports = {
       url: 'https://twitter.com/MoatazMohammady',
     },
   ],
-  certifications: [
-    {
+  certifications: [{
       place: 'Udacity',
       title: 'Web Development Professional',
       description: 'Nanodegree',
-      url:
-        'https://graduation-api.udacity.com/api/certificate/A57FXWSR/download',
+      url: 'https://graduation-api.udacity.com/api/certificate/A57FXWSR/download',
       issued: 'Aug 2020',
     },
     {
       place: 'Udacity',
       title: 'Front End Development Track',
       description: 'One Million Arab Coders',
-      url:
-        'https://s3-us-west-2.amazonaws.com/udacity-printer/production/certificates/8274026e-a9af-49ba-b82b-66b5824b65fe.pdf',
+      url: 'https://s3-us-west-2.amazonaws.com/udacity-printer/production/certificates/8274026e-a9af-49ba-b82b-66b5824b65fe.pdf',
       issued: 'May 2019',
     },
     {
       place: 'futurelearn.com',
       title: "BASIC ENGLISH 1: ELEMENTARY KING'S COLLEGE LONDON",
-      description:
-        'This course helped you develop your use of English in a range of everyday situations and built your confidence by improving your listening skills. You’ve heard people talking in a number of different situations and have had an opportunity to review the language through a series of quizzes.',
+      description: 'This course helped you develop your use of English in a range of everyday situations and built your confidence by improving your listening skills. You’ve heard people talking in a number of different situations and have had an opportunity to review the language through a series of quizzes.',
       url: 'https://www.futurelearn.com/certificates/tl15gwi',
       issued: 'Dec 2018',
     },
     {
       place: 'futurelearn.com',
       title: "BASIC ENGLISH 2: PRE-INTERMEDIATE KING'S COLLEGE LONDON",
-      description:
-        'This online course helped you to demonstrate understanding of language that can help you progress along the path to studying at a university.',
+      description: 'This online course helped you to demonstrate understanding of language that can help you progress along the path to studying at a university.',
       url: 'https://www.futurelearn.com/certificates/d829clk',
       issued: 'Jan 2020',
     },
     {
       place: 'Freecodecamp',
       title: 'Responsive Web Design',
-      description:
-        'Developer Certification, representing approximately 300 hours of coursework',
-      url:
-        'https://www.freecodecamp.org/certification/fcc319f679c/responsive-web-design',
+      description: 'Developer Certification, representing approximately 300 hours of coursework',
+      url: 'https://www.freecodecamp.org/certification/fcc319f679c/responsive-web-design',
       issued: 'Jun 2018',
     },
 
@@ -312,15 +311,13 @@ module.exports = {
       title: 'HTML Fundamentals course',
       url: 'https://www.sololearn.com/Certificate/1014-1342830/pdf/',
       issued: 'Jan 2017',
-    },
-    ,
+    }, ,
     {
       place: 'Sololearn',
       title: 'PHP Fundamentals course',
       url: 'https://www.sololearn.com/Certificate/1059-1342830/pdf/',
       issued: 'July 2016',
-    },
-    ,
+    }, ,
     {
       place: 'Sololearn',
       title: 'JavaScript Fundamentals course',
@@ -332,8 +329,7 @@ module.exports = {
   phone: '+201150064746',
   address: 'Mansouria,Giza,Egypt',
   location: 'https://goo.gl/maps/QHh4H4eYJMByuneC8',
-  jobs: [
-    {
+  jobs: [{
       company: 'Wuilt',
       url: 'http://wuilt.com/',
       period: 'Jan 2020 - Present',
@@ -344,20 +340,18 @@ module.exports = {
         'Collaborate with other team members and stakeholders',
       ],
       description: '',
-      projects: [
-        {
-          name: 'Wuilt.com',
-          url: 'https://app.wuilt.com/',
-          technologies: [
-            'PHP',
-            'Laravel',
-            'reactJS',
-            'nodeJS',
-            'mondoDB',
-            'graphql',
-          ],
-        },
-      ],
+      projects: [{
+        name: 'Wuilt.com',
+        url: 'https://app.wuilt.com/',
+        technologies: [
+          'PHP',
+          'Laravel',
+          'reactJS',
+          'nodeJS',
+          'mondoDB',
+          'graphql',
+        ],
+      }, ],
     },
     {
       company: 'camelCase',
@@ -370,8 +364,7 @@ module.exports = {
       period: 'May 2018 - Dec 2019',
       occupation: 'Full-Stack PHP Developer',
       description: '',
-      projects: [
-        {
+      projects: [{
           name: 'skilledmatrix.com',
           url: 'http://skilledmatrix.com/',
           technologies: ['PHP', 'Laravel'],
@@ -403,14 +396,12 @@ module.exports = {
         'Collaborate with other team members and stakeholders',
       ],
       description: '',
-      projects: [
-        {
-          name: 'netscribe',
-          url: 'https://viqsolutions.com/solutions/netscribe/',
+      projects: [{
+        name: 'netscribe',
+        url: 'https://viqsolutions.com/solutions/netscribe/',
 
-          technologies: ['angularjs'],
-        },
-      ],
+        technologies: ['angularjs'],
+      }, ],
     },
 
     {
@@ -424,8 +415,7 @@ module.exports = {
         'Conducting website performance tests.',
       ],
       description: '',
-      projects: [
-        {
+      projects: [{
           name: 'la creation',
           url: 'https://www.picuki.com/profile/la__creation',
           technologies: ['PHP', 'Wordpress'],
@@ -448,13 +438,11 @@ module.exports = {
         'Assure that all user input is validated before submitting to back-end',
       ],
       description: '',
-      projects: [
-        {
-          name: 'hotel reservation project',
-          url: '#',
-          technologies: ['angularjs'],
-        },
-      ],
+      projects: [{
+        name: 'hotel reservation project',
+        url: '#',
+        technologies: ['angularjs'],
+      }, ],
     },
     {
       company: 'Orchtech',
@@ -467,8 +455,7 @@ module.exports = {
         'Conducting website performance tests.',
       ],
       description: '',
-      projects: [
-        {
+      projects: [{
           name: 'sevensprings.ch',
           url: 'https://sevensprings.ch/',
 
