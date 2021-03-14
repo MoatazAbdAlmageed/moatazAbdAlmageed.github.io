3cf6e5a1e2bd529b9950284dcab8ef72b0e47033
Moataz Mohammady <moataz@wuilt.com>
2021-03-14 15:11:21 +0200

    wip

--- src/woot-wapper.js
+++ src/woot-wapper.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
-import Layout from './components/layout';
+import Layout from './components/Layout';
 import Footer from './components/Footer';
 
 export const wrapPageElement = ({ element }) => {
