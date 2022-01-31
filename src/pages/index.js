0ab0e4e1a29d8ea659dd009c527b0563ca62dd71
Moataz <moataz@wuilt.com>
2022-01-31 20:14:22 +0200

    replace sidebar with Navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,30 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 import About from '../components/About';
 import Awards from '../components/Awards';
 import Contact from '../components/Contact';
 import Experience from '../components/Experience';
 import Layout from '../components/Layout';
+import Navigation from '../components/Navigation';
 import Projects from '../components/Projects';
-import Sidebar from '../components/Sidebar';
 import TestimonialContent from '../components/Testimonials';
+// TODO:add sendstrap
+// const sendstrap = require('https://app.sendstrap.com/scripts/js/social_button.js?id=2505&key=eTpoTpDQz5fRdasrE9ZkEAXsNJqWNkrqPISZ1wLB');
+// console.log('\n'.repeat(5));
+// console.log('************ sendstrap **************');
+// console.log(sendstrap);
 const IndexPage = () => {
   return (
     <Layout>
-      <Flex>
-        <Box>
-          <Sidebar />
-        </Box>
-        <Box p={1}>
-          <About />
-          <Experience />
-          <Projects />
-          <TestimonialContent />
-          <Awards />
-          <Contact />
-        </Box>
-      </Flex>
+      <Navigation />
+      <Box mt={2}>
+        <About />
+        <Experience />
+        <Projects />
+        <TestimonialContent />
+        <Awards />
+        <Contact />
+      </Box>
     </Layout>
   );
 };
